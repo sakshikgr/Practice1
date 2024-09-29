d9ae834f9cbf3ce9dc447d1cba5937bc62cf4373
Sakshi Rajput <sakshikr@amazon.com>
2024-09-29 15:27:31 +0530

    HashCode & Equals method Implementation

--- src/main/java/org/example/Developer.java
+++ src/main/java/org/example/Developer.java
@@ -1,6 +1,35 @@
 package org.example;
 
 public class Developer implements Employee{
+    private int id;
+    private String name;
+    public Developer(int id, String name)
+    {
+        this.id=id;
+        this.name=name;
+    }
+    public Developer(){}
+    public String getName() {
+        return name;
+    }
+    @Override
+    public int hashCode() {
+        return id*10;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if(obj==this)
+        {
+            return true;
+        }
+        if(obj==null || obj.getClass()!=this.getClass())
+        {
+            return false;
+        }
+        Developer developer= (Developer) obj;
+        return developer.name.equals(this.name) && developer.id==this.id;
+    }
 
     @Override
     public void display() {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        Developer developer1=new Developer(6,"Sakshi");
+        Developer developer2=new Developer(5,"Omkar");
+        Developer developer3=new Developer(8,"Omkar");
+        HashMap<Developer, String>mp=new HashMap<>();
+        mp.put(developer1,"A grade");
+        mp.put(developer2,"B grade");
+        mp.put(developer3,"A grade");
+        System.out.println("Grade of Sakshi" + mp.get(developer1));
+        System.out.println("Grade of Omkar" + mp.get(developer2));
+        System.out.println("Grade of Omkar2" + mp.get(developer3));
+
+        printBucketIndex(developer1, developer2);
+        printBucketIndex(developer2, developer3);
+
+        printBucketIndex(developer2, developer1);
         Employee developer= new Developer();
         Employee it= new IT();
         developer.display();
@@ -9,4 +26,21 @@ public class Main {
         developer.displayOrg();
         it.displayOrg();
     }
+    // Method to calculate and print bucket index
+    public static void printBucketIndex(Developer developer, Developer otherDeveloper) {
+        // Get the hash code of the developer
+        int hashCode = developer.hashCode();
+
+        // Get the capacity (default is 16, but you could adjust it)
+        int capacity = 16; // HashMap initial capacity is typically 16
+        System.out.println("Hashcode for  " + developer.getName() + ": " + hashCode);
+
+        // Calculate bucket index using the formula
+        int bucketIndex = (hashCode & (capacity - 1));
+
+        System.out.println("Bucket index for " + developer.getName() + ": " + bucketIndex);
+        // Check equality with developer2
+        System.out.println("Is " + otherDeveloper.getName() + " equal to Omkar? " + developer.equals(otherDeveloper));
+    }
+
 }
